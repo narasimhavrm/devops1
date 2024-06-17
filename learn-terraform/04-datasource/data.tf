a4c7452e9fc06061ccf371498408baa82b099fc6
narasimhavrm <narasimhavrm@gmail.com>
2024-06-17 13:41:13 +0530

    data source in terraform

--- learn-terraform/04-datasource/data.tf
+++ learn-terraform/04-datasource/data.tf
@@ -7,7 +7,9 @@ data "aws_ec2_spot_price" "example" {
     values = ["Linux/UNIX"]
   }
 }
-
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+}
 output price {
   value = data.aws_ec2_spot_price.example.spot_price
 }
