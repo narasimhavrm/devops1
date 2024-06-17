22d67869a93d73e87f8e5eb52f066d4e09227bc0
narasimhavrm <narasimhavrm@gmail.com>
2024-06-17 14:45:55 +0530

    data source in terraform

--- learn-terraform/04-datasource/data.tf
+++ learn-terraform/04-datasource/data.tf
@@ -13,3 +13,11 @@ provider "aws" {
 output price {
   value = data.aws_ec2_spot_price.example.spot_price
 }
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
+output security_group_id {
+  value = data.aws_security_group.selected.id
+}
