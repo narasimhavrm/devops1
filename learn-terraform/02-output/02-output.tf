620fd2753b7a521ad0f57b7519340f4348286653
narasimhavrm <narasimhavrm@gmail.com>
2024-06-17 10:54:13 +0530

    create aws ec2 instance Terraform

---
+++ learn-terraform/02-output/02-output.tf
@@ -0,0 +1,12 @@
+output "Sample" {
+  value = "Hello World"
+}
+
+#Any string in terraform should be quoted in double quotes only.
+#single quote is not supported
+
+output "Sample1" {
+  value = 1000
+}
+
+#Numbers and booleans(true and false)
