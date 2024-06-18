4e158648384738de5685e673ecc48316f09012f9
narasimhavrm <narasimhavrm@gmail.com>
2024-06-18 08:45:47 +0530

    Create a module with one instance and one security group

--- learn-terraform/07-module-01/ec2/main.tf
+++ learn-terraform/07-module-01/ec2/main.tf
@@ -42,6 +42,6 @@ resource "aws_security_group" "sg" {
 
 variable "name" {}
 
-output public_ip {
+output "public_ip" {
   value = aws_instance.web.public_ip
 }
--- learn-terraform/07-module-01/main.tf
+++ learn-terraform/07-module-01/main.tf
@@ -7,3 +7,11 @@ module sample2 {
   source = "./ec2"
   name = "sample2"
 }
+
+output "sample1_public_ip" {
+  value = module.sample1.public_ip
+}
+
+output "sample2_public_ip" {
+  value = module.sample2.public_ip
+}
