8163eba6a3d031a9373586a5bb7e224ec23a6e31
narasimhavrm <narasimhavrm@gmail.com>
2024-06-18 07:55:30 +0530

    data source in terraform

--- learn-terraform/04-datasource/data.tf
+++ learn-terraform/04-datasource/data.tf
@@ -21,3 +21,14 @@ data "aws_security_group" "selected" {
 output security_group_id {
   value = data.aws_security_group.selected.id
 }
+
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+
+}
+
+output ami_id {
+  value = data.aws_ami.example.id
+}
