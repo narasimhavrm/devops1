9b2fd944e891d0e1e91420dbdf919957ff97ccb1
narasimhavrm <narasimhavrm@gmail.com>
2024-06-18 09:19:01 +0530

    Provisioner in terraform

--- learn-terraform/08-provisioner/main.tf
+++ learn-terraform/08-provisioner/main.tf
@@ -1,11 +1,25 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-002625eba7df10194"]
 
   tags = {
     Name = "HelloWorld"
   }
+    provisioner "remote-exec" {
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = aws_instance.web.public_ip
+      }
+      inline = [
+        "sudo labauto ansible",
+        "ansible-pull -i localhost, -U https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=frontend",
+      ]
+    }
 }
+
 
 data "aws_ami" "example" {
   owners = ["973714476881"]
@@ -13,15 +27,6 @@ data "aws_ami" "example" {
   name_regex       = "Centos-8-DevOps-Practice"
 
 }
-#   provisioner "remote-exec" {
-#     connection {
-#       type     = "ssh"
-#       user     = "centos"
-#       password = "DevOps321"
-#       host     = aws_instance.web.public_ip
-#     }
-#     inline = [
-#       "sudo labauto ansible",
-#       "ansible-pull -i localhost, -U https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=frontend",
-#     ]
-#   }
+
+
+
