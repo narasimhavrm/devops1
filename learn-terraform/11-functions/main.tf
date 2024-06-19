df90627c275f5d4fde3345a5e98ff98178e4a882
narasimhavrm <narasimhavrm@gmail.com>
2024-06-19 12:24:38 +0530

    functions in terrform

--- learn-terraform/11-functions/main.tf
+++ learn-terraform/11-functions/main.tf
@@ -5,3 +5,11 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable fruits {
+  default = ["mango", "orange", "avacado", "apple"]
+}
+
+output "countOfFruits" {
+  value = length(var.fruits)
+}
