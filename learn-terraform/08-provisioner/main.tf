92e13fbc021ab4667d0baf8d7d0cd91e1cd95d8c
narasimhavrm <narasimhavrm@gmail.com>
2024-06-19 09:59:53 +0530

    Provisioner in terraform

--- learn-terraform/08-provisioner/main.tf
+++ learn-terraform/08-provisioner/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "web" {
       }
       inline = [
         "sudo labauto ansible",
-        "ansible-pull -i localhost, -U https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=frontend",
+        "ansible-pull -i localhost, -U https://github.com/narasimhavrm/devops1.git main.yml -e env=dev -e role_name=frontend",
       ]
     }
 }
