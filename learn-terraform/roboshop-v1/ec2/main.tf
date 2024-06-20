395688267adb3af9f7bb83da72511f0015eb2d0a
narasimhavrm <narasimhavrm@gmail.com>
2024-06-20 14:52:31 +0530

    tag var.name

--- learn-terraform/roboshop-v1/ec2/main.tf
+++ learn-terraform/roboshop-v1/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = var.name
   }
 }
 resource "null_resource" "ansible" {
