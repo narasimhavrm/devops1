56eaa2fd974624f0e9651335106caa6c34643fad
narasimhavrm <narasimhavrm@gmail.com>
2024-06-21 18:11:24 +0530

    in root module env is var.env

---
+++ roboshop-terraform/env-dev/state.tf
@@ -0,0 +1,3 @@
+bucket = "myterraformstatefilebucket"
+key    = "dev/terraform.tfstate"
+region = "us-east-1"
---
+++ roboshop-terraform/env-prod/state.tf
@@ -0,0 +1,3 @@
+bucket = "myterraformstatefilebucket"
+key    = "prod/terraform.tfstate"
+region = "us-east-1"
