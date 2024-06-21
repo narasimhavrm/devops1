c4314cdc066b420d96547f55553767fe1be2a8f1
narasimhavrm <narasimhavrm@gmail.com>
2024-06-21 14:25:17 +0530

    making param changes in roboshop conf file in roboshop ansible project.

--- learn-terraform/aws-param-store/main.tf
+++ learn-terraform/aws-param-store/main.tf
@@ -7,11 +7,11 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.mystorymyway.online", type = "String"},
-    {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.mystorymyway.online", type = "String"},
-    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.mystorymyway.online", type = "String"},
-    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.mystorymyway.online", type = "String"},
-    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.mystorymyway.online", type = "String"},
+    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.mystorymyway.online:8080/", type = "String"},
+    {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.mystorymyway.online:8080/", type = "String"},
+    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.mystorymyway.online:8080/", type = "String"},
+    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.mystorymyway.online:8080/", type = "String"},
+    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.mystorymyway.online:8080/", type = "String"},
   ]
 
 }
