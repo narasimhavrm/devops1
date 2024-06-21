65f86e1c50cc7b5bb394d079cc7ba63f8f6b0902
narasimhavrm <narasimhavrm@gmail.com>
2024-06-21 19:22:03 +0530

    build of dev and prod main and tf module

--- roboshop-terraform/env-prod/main.tf
+++ roboshop-terraform/env-prod/main.tf
@@ -1,1 +1,16 @@
 env = "prod"
+
+components =  {
+
+  frontend  = {}
+  mongodb   = {}
+  catalogue = {}
+  redis     = {}
+  user      = {}
+  cart      = {}
+  mysql     = {}
+  shipping  = {}
+  rabbitmq  = {}
+  payment   = {}
+
+}
--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -1,4 +1,11 @@
-module "test" {
-  source = "git::https://github.com/narasimhavrm/devops1.git//tf-module-app"
+module "instances" {
+  for_each = var.components
+  source   = "git::https://github.com/narasimhavrm/devops1.git//tf-module-app"
+  component     = each.key
   env = var.env
 }
+
+# module "test" {
+#   source = "git::https://github.com/narasimhavrm/devops1.git//tf-module-app"
+#   env = var.env
+# }
