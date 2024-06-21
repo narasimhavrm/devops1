b2055481a57aafeccf5e5f9f2543da9a5ab531c6
narasimhavrm <narasimhavrm@gmail.com>
2024-06-21 13:54:53 +0530

    roboshop frontend url params

--- learn-terraform/aws-param-store/main.tf
+++ learn-terraform/aws-param-store/main.tf
@@ -7,8 +7,19 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    {name = "demo1", value = "demo1", type = "String"},
-    {name = "demo2", value = "demo2", type = "String"}
+    {name = "roboshop.dev.frontend.catalogue", value = "http://catalogue-dev.mystorymyway.online", type = "String"},
+    {name = "roboshop.dev.frontend.user", value = "http://user-dev.mystorymyway.online", type = "String"},
+    {name = "roboshop.dev.frontend.cart", value = "http://cart-dev.mystorymyway.online", type = "String"},
+    {name = "roboshop.dev.frontend.payment", value = "http://payment-dev.mystorymyway.online", type = "String"},
+    {name = "roboshop.dev.frontend.shipping", value = "http://shipping-dev.mystorymyway.online", type = "String"},
   ]
 
 }
+
+# variable "params" {
+#   default = [
+#     {name = "demo1", value = "demo1", type = "String"},
+#     {name = "demo2", value = "demo2", type = "String"}
+#   ]
+#
+# }
