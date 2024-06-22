8a159561430dfb8610d3d61b6e68923933cb4704
narasimhavrm <narasimhavrm@gmail.com>
2024-06-22 12:08:12 +0530

    removing set-hostname

--- tf-module-app/main.tf
+++ tf-module-app/main.tf
@@ -106,7 +106,6 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo labauto ansible",
-      "sudo set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/narasimhavrm/devops1.git roboshop-ansible/main.yml -e env=${var.env} -e role_name=${var.component}"
     ]
   }
