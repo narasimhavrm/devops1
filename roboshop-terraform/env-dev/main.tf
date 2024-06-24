60b4a1af73d39a6bcc89465db3f27d08ccf13528
narasimhavrm <narasimhavrm@gmail.com>
2024-06-24 21:58:33 +0530

    tag for each value

--- roboshop-terraform/env-dev/main.tf
+++ roboshop-terraform/env-dev/main.tf
@@ -3,34 +3,34 @@ env = "dev"
 components =  {
 
   frontend  = {
-    tags = { Monitor = "true", env = "dev"}
+    tags = { monitor = "true", env = "dev"}
   }
   mongodb   = {
     tags = { env = "dev"}
   }
   catalogue = {
-    tags = { Monitor = "true", env = "dev"}
+    tags = { monitor = "true", env = "dev"}
   }
   redis     = {
     tags = {  env = "dev"}
   }
   user      = {
-    tags = { Monitor = "true", env = "dev"}
+    tags = { monitor = "true", env = "dev"}
   }
   cart      = {
-    tags = { Monitor = "true", env = "dev"}
+    tags = { monitor = "true", env = "dev"}
   }
   mysql     = {
     tags = { env = "dev"}
   }
   shipping  = {
-    tags = { Monitor = "true", env = "dev"}
+    tags = { monitor = "true", env = "dev"}
   }
   rabbitmq  = {
     tags = { env = "dev"}
   }
   payment   = {
-    tags = { Monitor = "true", env = "dev"}
+    tags = { monitor = "true", env = "dev"}
   }
 
 }
--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -3,6 +3,7 @@ module "instances" {
   source   = "git::https://github.com/narasimhavrm/devops1.git//tf-module-app"
   component     = each.key
   env = var.env
+  tags = each.value["tags"]
 }
 
 # module "test" {
