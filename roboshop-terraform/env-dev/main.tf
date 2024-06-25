f52d619994dd346a8768acadf572de762d912165
narasimhavrm <narasimhavrm@gmail.com>
2024-06-25 21:05:28 +0530

    name tag

--- roboshop-terraform/env-dev/main.tf
+++ roboshop-terraform/env-dev/main.tf
@@ -1,13 +1,5 @@
 env = "dev"
 
-tags = {
-  company_name  = "XYZ Tech"
-  business      = "ecommerce"
-  business_unit = "retail"
-  cost_center   = "322"
-  project_name  = "roboshop"
-}
-
 components =  {
 
   frontend  = {
