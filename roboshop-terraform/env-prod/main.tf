96dbe430d8fefaa04d81039234e94a2db6ec514c
narasimhavrm <narasimhavrm@gmail.com>
2024-06-26 14:01:05 +0530

    tags in prod

--- roboshop-terraform/env-prod/main.tf
+++ roboshop-terraform/env-prod/main.tf
@@ -19,3 +19,12 @@ vpc = {
   main = {
     cidr_block = "10.20.0.0/16"
   }
+}
+
+tags = {
+  company_name = "ABC Company"
+  business = "ecomm"
+  business_unit = "APAC"
+  cost_center = "322"
+  project_name = "myproject"
+}
