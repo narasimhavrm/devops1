ec7ac895ca84d372960f7958fc5219920d1dc9fe
narasimhavrm <narasimhavrm@gmail.com>
2024-06-26 15:37:49 +0530

    var subnets for each in vpc main

--- tf-module-vpc/main.tf
+++ tf-module-vpc/main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "main" {
 module subnets {
   source = "./subnets"
   vpc_id = aws_vpc.main.id
-  for_each = each.subnets
+  for_each = var.subnets
   cidr_block = each.value["cidr_block"]
   subnet_name = each.key
   env = var.env
