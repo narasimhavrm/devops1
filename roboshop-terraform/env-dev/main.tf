be06995ebd450b6046bb57059910bd65cf390af3
narasimhavrm <narasimhavrm@gmail.com>
2024-06-27 14:59:33 +0530

    rabbitmq creation including subnets

--- roboshop-terraform/env-dev/main.tf
+++ roboshop-terraform/env-dev/main.tf
@@ -58,6 +58,7 @@ vpc = {
 
 default_vpc_id = "vpc-07c5e9a858a2f95c1"
 default_vpc_rt = "rtb-035d7d93dadf94f09"
+allow_ssh_cidr = ["172.31.16.108/32"]
 
 rabbitmq = {
   main = {
--- roboshop-terraform/variables.tf
+++ roboshop-terraform/variables.tf
@@ -5,4 +5,4 @@ variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_vpc_rt" {}
 variable "rabbitmq" {}
-
+variable "allow_ssh_cidr" {}
