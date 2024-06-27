a28472a1ace7eb54fa4a1a062afa7eb910756ce8
narasimhavrm <narasimhavrm@gmail.com>
2024-06-27 12:15:26 +0530

    route association

--- tf-module-vpc/subnets/main.tf
+++ tf-module-vpc/subnets/main.tf
@@ -15,3 +15,10 @@ resource "aws_route_table" "table" {
   tags = merge({Name = "${var.env}-${var.subnet_name}"
   }, var.tags)
 }
+
+resource "aws_route_table_association" "association" {
+  count = length(aws_subnet.main.*.id)
+  subnet_id = element(aws_subnet.main.*.id, count.index )
+  route_table_id = aws_route_table.table.id
+
+}
