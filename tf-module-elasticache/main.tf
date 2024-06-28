bd8500e90c8a17904e8801153cd34730ac087b01
narasimhavrm <narasimhavrm@gmail.com>
2024-06-28 11:15:55 +0530

    skip rds cluster snapshot

--- tf-module-elasticache/main.tf
+++ tf-module-elasticache/main.tf
@@ -4,3 +4,12 @@ resource "aws_elasticache_subnet_group" "main" {
 
   tags = merge({ Name = "${var.component}-${var.env}" }, var.tags )
 }
+
+# resource "aws_elasticache_cluster" "example" {
+#   cluster_id           = "${var.component}-${var.env}"
+#   engine               = var.engine
+#   node_type            = var.node_type
+#   num_cache_nodes      = var.num_cache_nodes
+#   engine_version       = var.engine_version
+#   port                 = var.port
+# }
