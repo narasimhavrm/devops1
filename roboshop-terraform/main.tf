219853a2d923e816c6d6f730b8f314bee0609c55
narasimhavrm <narasimhavrm@gmail.com>
2024-06-28 10:07:30 +0530

    instance count

--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -49,6 +49,7 @@ module "rds" {
   engine_version = each.value["engine_version"]
   database_name = each.value["database_name"]
   instance_class = each.value["instance_class"]
+  instance_count = each.value["instance_count"]
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
   vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
