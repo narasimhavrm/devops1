0132a6481083f5dd62dc40be11b6c532cfe9ee5f
narasimhavrm <narasimhavrm@gmail.com>
2024-06-30 16:12:09 +0530

    prod app creation

--- learn-terraform/aws-param-store/main.tf
+++ learn-terraform/aws-param-store/main.tf
@@ -52,6 +52,49 @@ variable "params" {
     {name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
     { name = "roboshop.dev.user.docdb_endpoint", value = "docdb-dev.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com", type = "String" }
 
+  ### PROD PARAMS
+    {name = "roboshop.prod.frontend.catalogue_url", value = "http://catalogue-prod.mystorymyway.online/", type = "String"},
+    {name = "roboshop.prod.frontend.user_url", value = "http://user-prod.mystorymyway.online/", type = "String"},
+    {name = "roboshop.prod.frontend.cart_url", value = "http://cart-prod.mystorymyway.online/", type = "String"},
+    {name = "roboshop.prod.frontend.payment_url", value = "http://payment-prod.mystorymyway.online/", type = "String"},
+    {name = "roboshop.prod.frontend.shipping_url", value = "http://shipping-prod.mystorymyway.online/", type = "String"},
+
+    {name = "roboshop.prod.cart.redis_host", value = "elasticache-prod.iiya6y.clustercfg.use1.cache.amazonaws.com", type = "String"},
+    {name = "roboshop.prod.cart.catalogue_host", value = "catalogue-prod.mystorymyway.online", type = "String"},
+    {name = "roboshop.prod.cart.catalogue_port", value = "80", type = "String"},
+    {name = "roboshop.prod.catalogue.mongo", value = "true", type = "String"},
+    #{name = "roboshop.prod.catalogue.mongo_url", value = "mongodb://mongodb-prod.mystorymyway.online:27017/catalogue", type = "String"},
+
+
+    {name = "roboshop.prod.payment.cart_host", value = "cart-prod.mystorymyway.online", type = "String"},
+    {name = "roboshop.prod.payment.cart_port", value = "80", type = "String"},
+    {name = "roboshop.prod.payment.user_host", value = "user-prod.mystorymyway.online", type = "String"},
+    {name = "roboshop.prod.payment.user_port", value = "80", type = "String"},
+    {name = "roboshop.prod.payment.amqp_host", value = "rabbitmq-prod.mystorymyway.online", type = "String"},
+    {name = "roboshop.prod.payment.amqp_user", value = "roboshop", type = "String"},
+    {name = "roboshop.prod.shipping.cart_endpoint", value = "cart-prod.mystorymyway.online:80", type = "String"},
+    {name = "roboshop.prod.shipping.db_host", value = "rds-prod.cluster-chcio4eskuv8.us-east-1.rds.amazonaws.com", type = "String"},
+    {name = "roboshop.prod.user.mongo", value = "true", type = "String"},
+    #{name = "roboshop.prod.user.redis_host", value = "redis-prod.mystorymyway.online", type = "String"},
+    {name = "roboshop.prod.user.redis_host", value = "elasticache-prod.iiya6y.clustercfg.use1.cache.amazonaws.com", type = "String"},
+    #{name = "roboshop.prod.user.mongo_url", value = "mongodb://mongodb-prod.mystorymyway.online:27017/users", type = "String"},
+    {name = "roboshop.prod.rabbitmq.amqp_user", value = "roboshop", type = "String"},
+    {name = "roboshop.prod.mysql.username", value = "roboshop", type = "String"},
+    {name = "roboshop.prod.docdb.username", value = "roboshop", type = "String"},
+    {name = "roboshop.prod.mysql.endpoint", value = "rds-prod.cluster-chcio4eskuv8.us-east-1.rds.amazonaws.com", type = "String"},
+
+
+
+    #Passwords will be never part of git repos, Usually in organizations we create them manually,
+    # whom ever have access will provision in parameter store
+    {name = "roboshop.prod.payment.amqp_pass", value = "roboshop123", type = "SecureString"},
+    {name = "roboshop.prod.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString"},
+    {name = "roboshop.prod.mysql.password", value = "roboshop123", type = "SecureString"},
+    {name = "roboshop.prod.docdb.password", value = "roboshop123", type = "SecureString"},
+    {name = "roboshop.prod.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.prod.catalogue.docdb_endpoint", value = "docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com", type = "String" },
+    {name = "roboshop.prod.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.prod.user.docdb_endpoint", value = "docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com", type = "String" }
 
 
   ]
--- roboshop-terraform/env-prod/main.tf
+++ roboshop-terraform/env-prod/main.tf
@@ -2,23 +2,37 @@ env = "prod"
 
 components =  {
 
-  frontend  = {}
-  mongodb   = {}
-  catalogue = {}
-  redis     = {}
-  user      = {}
-  cart      = {}
-  mysql     = {}
-  shipping  = {}
-  rabbitmq  = {}
-  payment   = {}
-
-}
-
-vpc = {
-  main = {
-    cidr_block = "10.20.0.0/16"
+  frontend  = {
+    tags = { monitor = "true", env = "dev"}
   }
+  mongodb   = {
+    tags = { env = "dev"}
+  }
+  catalogue = {
+    tags = { monitor = "true", env = "dev"}
+  }
+  redis     = {
+    tags = {  env = "dev"}
+  }
+  user      = {
+    tags = { monitor = "true", env = "dev"}
+  }
+  cart      = {
+    tags = { monitor = "true", env = "dev"}
+  }
+  mysql     = {
+    tags = { env = "dev"}
+  }
+  shipping  = {
+    tags = { monitor = "true", env = "dev"}
+  }
+  rabbitmq  = {
+    tags = { env = "dev"}
+  }
+  payment   = {
+    tags = { monitor = "true", env = "dev"}
+  }
+
 }
 
 tags = {
@@ -28,3 +42,166 @@ tags = {
   cost_center = "322"
   project_name = "myproject"
 }
+
+vpc = {
+  main = {
+    cidr_block = "10.10.0.0/16"
+    ##web_subnet_cidr_block = ["10.0.0.0/24"]
+    subnets = {
+      web = { cidr_block = ["10.10.0.0/24", "10.10.1.0/24"] }
+      app = { cidr_block = ["10.10.2.0/24", "10.10.3.0/24"] }
+      db = { cidr_block = ["10.10.4.0/24", "10.10.5.0/24"] }
+      public = { cidr_block = ["10.10.6.0/24", "10.10.7.0/24"] }
+    }
+  }
+}
+
+default_vpc_id = "vpc-07c5e9a858a2f95c1"
+default_vpc_rt = "rtb-035d7d93dadf94f09"
+allow_ssh_cidr = ["172.31.16.108/32"]
+zone_id = "Z034092834K6LW0HQ9HDN"
+kms_key_id = "af887b98-0e7c-49a1-93eb-aeaab4189fc3"
+kms_key_arn = "arn:aws:kms:us-east-1:079329262703:key/af887b98-0e7c-49a1-93eb-aeaab4189fc3"
+allow_prometheus_cidr = ["172.31.55.183/32"]
+
+
+rabbitmq = {
+  main = {
+    instance_type = "t2.micro"
+    component = "rabbitmq"
+
+  }
+}
+
+rds = {
+  main = {
+    component      = "rds"
+    engine                  = "aurora-mysql"
+    engine_version          = "5.7.mysql_aurora.2.11.3"
+    database_name           = "dummy"
+    instance_class          = "db.t3.small"
+    instance_count = 1
+
+  }
+}
+
+documentdb = {
+  main = {
+    component      = "docdb"
+    engine = "docdb"
+    engine_version = "4.0.0"
+    instance_class = "db.t3.medium"
+    db_instance_count = 1
+
+
+  }
+}
+
+elasticache = {
+  main = {
+    component      = "elasticache"
+    engine = "redis"
+    engine_version = "6.x"
+    replicas_per_node_group = 1
+    num_node_groups = 1
+    node_type = "cache.t3.micro"
+    parameter_group_name = "default.redis6.x.cluster.on"
+
+  }
+}
+
+alb = {
+  public = {
+    name = "public"
+    internal = false
+    load_balancer_type = "application"
+    subnet_ref = "public"
+
+  }
+  private = {
+    name = "private"
+    internal = true
+    load_balancer_type = "application"
+    subnet_ref = "app"
+
+  }
+}
+
+apps = {
+  cart = {
+    component = "cart"
+    app_port = 8080
+    instance_type = "t2.micro"
+    desired_capacity = 1
+    max_size = 5
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 100
+
+  }
+  user = {
+    component = "user"
+    app_port = 8080
+    instance_type = "t2.micro"
+    desired_capacity = 1
+    max_size = 5
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 101
+    extra_param_access = ["arn:aws:ssm:us-east-1:079329262703:parameter/roboshop.prod.docdb.*"]
+  }
+  catalogue = {
+    component = "catalogue"
+    app_port = 8080
+    instance_type = "t2.micro"
+    desired_capacity = 1
+    max_size = 5
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 102
+    extra_param_access = ["arn:aws:ssm:us-east-1:079329262703:parameter/roboshop.prod.docdb.*"]
+  }
+  payment = {
+    component = "payment"
+    app_port = 8080
+    instance_type = "t2.micro"
+    desired_capacity = 1
+    max_size = 5
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 103
+
+
+  }
+  shipping = {
+    component = "shipping"
+    app_port = 8080
+    instance_type = "t2.micro"
+    desired_capacity = 1
+    max_size = 5
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 104
+    extra_param_access = ["arn:aws:ssm:us-east-1:079329262703:parameter/roboshop.prod.mysql.*"]
+
+  }
+  frontend = {
+    component = "frontend"
+    app_port = 80
+    instance_type = "t2.micro"
+    desired_capacity = 1
+    max_size = 5
+    min_size = 1
+    subnet_ref = "web"
+    lb_ref = "public"
+    lb_rule_priority = 100
+  }
+
+
+}
+
