88829111492fed52c4ee821f3667bb64d8e5048b
narasimhavrm <narasimhavrm@gmail.com>
2024-06-30 11:42:44 +0530

    rabbit mq decrypt permission with kms key arn

--- tf-module-rabbitmq/iam.tf
+++ tf-module-rabbitmq/iam.tf
@@ -14,9 +14,12 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
-          "ssm:GetParameter"
+          "ssm:GetParameter",
+          "kms:Decrypt"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:079329262703:parameter/roboshop.${var.env}.${var.component}.*"
+        "Resource": [
+          "arn:aws:ssm:us-east-1:079329262703:parameter/roboshop.${var.env}.${var.component}.*",
+          var.kms_key_arn ]
       }
     ]
   })
