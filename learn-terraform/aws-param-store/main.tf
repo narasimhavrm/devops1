12322f33dd7816cc4ba7ab546a979a0d9c489c4e
narasimhavrm <narasimhavrm@gmail.com>
2024-07-03 14:51:50 +0530

    aws param no comma

--- learn-terraform/aws-param-store/main.tf
+++ learn-terraform/aws-param-store/main.tf
@@ -94,7 +94,7 @@ variable "params" {
     {name = "roboshop.prod.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
     { name = "roboshop.prod.catalogue.docdb_endpoint", value = "docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com", type = "String" },
     {name = "roboshop.prod.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
-    { name = "roboshop.prod.user.docdb_endpoint", value = "docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com", type = "String" }
+    { name = "roboshop.prod.user.docdb_endpoint", value = "docdb-prod.cluster-chcio4eskuv8.us-east-1.docdb.amazonaws.com", type = "String" },
 
     ##Nexus
     {name = "roboshop.nexus.username", value = "admin", type = "String"},
